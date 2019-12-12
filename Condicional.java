d4f50a2f11dc74bb2dc04dce053822aa39f99771
josed126 <clavellino1993@gmail.com>
2019-12-12 15:49:15 +0100

    Añado el segundo

---
+++ Condicional.java
@@ -0,0 +1,12 @@
+public class Condicional {
+	public static void main (String[] args) {
+		System.out.print("Cual es tu nombre");
+		String respuesta = System.console().readLine();
+			if (respuesta = Madrid) {
+					System.out.println("Es corecto");
+			} else {
+					System.out.println("Es incorrecto");
+
+		}
+	}
+}
